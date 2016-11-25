b3f1e057a6e54dea8192707562798a27b0f0db55
Ridzuan Mohamad <ridzuanmo@gmail.com>
2016-11-25 16:54:55 +0800

    add legend

--- plot3.R
+++ plot3.R
@@ -13,4 +13,7 @@ plot(dateTime, Sub_metering_1, type="l", xlab=" ", ylab="Energy sub metering")
 points(dateTime, Sub_metering_2, type="l", col="red")
 points(dateTime, Sub_metering_3, type="l", col="blue")
 
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col=c("black", "red", "blue"), lty=1)
+
 dev.off()
--- plot4.R
+++ plot4.R
@@ -26,6 +26,9 @@ plot(dateTime, Sub_metering_1, type="l", xlab=" ", ylab="Energy sub metering")
 points(dateTime, Sub_metering_2, type="l", col="red")
 points(dateTime, Sub_metering_3, type="l", col="blue")
 
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col=c("black", "red", "blue"), lty=1)
+
 # This will plot into second column and bottom row
 plot(dateTime, globalReactivePower, type="l", xlab="datetime", ylab="Global_rective_power")
 
